3f3e3a45df088575b050cd1c8295e299fac01af5
oktavia18 <viaokta11223@gmail.com>
2023-11-16 14:45:51 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,42 @@
+# Akses list
+list1 = ["2", "4", "6", "8", "10"]
+print("Tampilkan element ke-3 :", list1[3]) 
+print("Ambil nilai element 2-4", list1[2:5])
+print("Ambil element terakhir :", list1[0:4])
+
+print( 45*"=", "\n")
+
+# Ubah element
+list2 = [3, 5, 9, 1, 11]
+print(list2)
+list2[4] = "Element 4"
+print(list2)
+
+list3 = ["Lima", "Empat", "Tiga", "Delapan", "Sepuluh"]
+print("Sebelum di ubah :", list3)
+list3[0:6] = [1, 2, 3, 4, 5]
+print("Setelah di ubah :", list3, "\n")
+
+# Tambah list element
+#  Index   0(-5),   1(-4),  2(-3),   3(-2),   4(-1) 
+lista  = [1, 2, 3, 4, 5]
+print("ListA\n", lista)
+listb  = [6, 7, 8]
+print("ListB Sebelum di tambahkan :\n", listb, "\n")
+
+lista  = [1 ,2 ,3 ,4 ,5]
+print("ListA\n", lista)
+listb  = [6 ,7 ,8 ]
+listb.insert(2, lista[0:2])  
+print("ListB Sesudah di tambahkan :\n", listb, "\n")
+
+# Mendambahkan list B dengan nilai string
+listb.append("Timey")
+print("ListB :\n", listb,"\n")
+
+# Menambahkan list B dengan 3 Nilai
+listb.extend([10, 12, 14])
+print("Mendambahkan ListB dengan 3 nilai :\n", listb,"\n")
+# Menggabungkan ListA dan ListB 
+listN = lista + listb
+print("Gabungan listA & ListB: \n", listN)
